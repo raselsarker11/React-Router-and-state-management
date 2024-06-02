5d6e6915a081f50093d896c436db6c0d301fe061
Your Name <your.email@example.com>
2024-06-02 16:15:48 +0600

    first commit

---
+++ price-options/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1 className='text-6xl bg-red-500'>Price options</h1>
+
+      
+    </>
+  )
+}
+
+export default App
